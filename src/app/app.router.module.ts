6cec9df2143c8ba15f83e0c234750d364e23053e
ashley945 <ashley9457@gmail.com>
2021-04-12 03:12:24 +0900

    monee push 04-12(2)

--- src/app/app.router.module.ts
+++ src/app/app.router.module.ts
@@ -3,10 +3,10 @@ import {LoginComponent} from './login/login.component';
 import {MainComponent} from './main/main.component';
 
 const AppRoutes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // 첫 화면을 login 페이지로 설정
-  { path: 'login', component: LoginComponent, }, // url 경로가 /login 일때 LoginComponent를 보여준다.
-  { path: 'main', component: MainComponent, }, // url 경로가 /main 일때 MainComponent를 보여준다.
-  { path: '**', redirectTo: '/login', pathMatch: 'full' }, // 잘못된 URL을 사용했을때 Login 페이지로 돌려보냄.
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, },
+  { path: 'main', component: MainComponent, },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 export const AppRouterModule = RouterModule.forRoot(AppRoutes, {useHash: true});
